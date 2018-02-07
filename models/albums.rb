71454a1d47b86c6c64b6cc49218254c6b17ff680
Jean BaMaung <jbamaung17@gmail.com>
2018-02-07 14:42:30 +0000

    edited album.rb

--- models/albums.rb
+++ models/albums.rb
@@ -5,11 +5,13 @@ require_relative('./artists.rb')
 
 class Album.new
 
-  attr_reader :id, :name
+  attr_reader :id, :name, :genre, :artist_id
 
   def initialize(options)
     @id = options['id'].to_i
     @name = options['name']
+    @genre = options['genre']
+    @artist_id = options['artist_id'].to_i
   end
 
 
