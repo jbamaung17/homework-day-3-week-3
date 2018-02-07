b22d9d21c5a61f79b34fbd63501e464417a7ddbf
Jean BaMaung <jbamaung17@gmail.com>
2018-02-07 16:02:14 +0000

    added an album method for artists

--- models/artists.rb
+++ models/artists.rb
@@ -23,6 +23,11 @@ class Artist
     SqlRunner.run(sql)
   end
 
-  
+  def album()
+    sql = "SELECT * FROM albums WHERE artist_id = $1"
+    values = [@id]
+    albums = SqlRunner.run(sql, values)
+    return albums.map{|album|Album.new(album)}
+  end
 
 end
